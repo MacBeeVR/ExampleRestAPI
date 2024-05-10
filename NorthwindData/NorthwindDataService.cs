98a3346cdec0aa9dc646051191db5571b9d4b38f
MacBeeVR <codyjseals@gmail.com>
2024-05-10 15:07:38 -0400

    Forgot to Include in the Last Commit. Added Demographics Relations to Get Queries for Customers. Changed Up DataService Validations. Added Comments.

--- NorthwindData/NorthwindDataService.cs
+++ NorthwindData/NorthwindDataService.cs
@@ -14,21 +14,21 @@ namespace NorthwindData
         #endregion
 
         #region Customers
-        Task<Customer?>         GetCustomerAsync(string id, bool includeRelations = false);
-        Task<List<Customer>>    GetCustomersAsync(bool includeRelations = false);
+        Task<Customer?>         GetCustomerAsync(string id);
+        Task<List<Customer>>    GetCustomersAsync();
         Task<bool>              AddCustomerAsync(Customer customer);
         Task<bool>              UpdateCustomerAsync(Customer customer);
         Task<bool>              DeleteCustomerAsync(string id);
 
-        Task<bool>              AddCustomerDemographicForCustomerAsync(string customerID, string customerTypeID);
-        Task<bool>              AddCustomerDemographicForCustomerAsync(string customerID, CustomerDemographics demographics);
+        Task<bool>              AddDemographicToCustomer(string customerID, string customerTypeID);
+        Task<bool>              DeleteDemographicFromCustomer(string customerID, string customerTypeID);
         #endregion
 
         #region Customer Demographics
-        Task<CustomerDemographics?>         GetCustomerDemographicsAsync(string id);
-        Task<List<CustomerDemographics>>    GetCustomerDemographicsAsync();
-        Task<bool>                          AddCustomerDemographicsAsync(CustomerDemographics demographics);
-        Task<bool>                          UpdateCustomerDemographicsAsync(CustomerDemographics demographics);
+        Task<CustomerDemographic?>         GetCustomerDemographicsAsync(string id);
+        Task<List<CustomerDemographic>>    GetCustomerDemographicsAsync();
+        Task<bool>                          AddCustomerDemographicsAsync(CustomerDemographic demographics);
+        Task<bool>                          UpdateCustomerDemographicsAsync(CustomerDemographic demographics);
         Task<bool>                          DeleteCustomerDemographicsAsync(string id);
         #endregion
     }
@@ -134,34 +134,104 @@ namespace NorthwindData
         #endregion
 
         #region Customers
-        public async Task<Customer?> GetCustomerAsync(string id, bool includeRelations = false)
+        /// <summary>
+        /// Returns true if Customer has the specified Demographic Added
+        /// </summary>
+        /// <param name="customerID">ID of the Customer to Check</param>
+        /// <param name="customerTypeID">ID of the Demographic to Find</param>
+        /// <returns>True if Demographic is Added to Customer, False Otherwise</returns>
+        private async Task<bool> CustomerHasDemographic(string customerID, string customerTypeID)
         {
             using (var connection = _ConnectionManager.GetConnection())
             {
                 await connection.OpenAsync();
-                var customer = await connection.QuerySingleOrDefaultAsync<Customer>("SELECT * FROM Customers WHERE CustomerID = @id", new { id });
+
+                var parameters  = new { customerID, customerTypeID };
+                var sql         = @"SELECT CustomerTypeID FROM CustomerCustomerDemo WHERE CustomerID = @customerID AND CustomerTypeID = @customerTypeID";
+                var result      = await connection.QuerySingleOrDefaultAsync<string>(sql, parameters);
+
+                return !string.IsNullOrWhiteSpace(result);
+            }
+        }
+
+        /// <summary>
+        /// Gets the Customer Record with the Specified ID
+        /// </summary>
+        /// <param name="id">ID of the Customer to Retrieve</param>
+        /// <returns>Customer Record if Exists, Null if Not</returns>
+        public async Task<Customer?> GetCustomerAsync(string id)
+        {
+            using (var connection = _ConnectionManager.GetConnection())
+            {
+                await connection.OpenAsync();
+
+                var query = @"SELECT C.*, '-', CD.* FROM Customers C
+                              INNER JOIN CustomerCustomerDemo CCD ON C.CustomerID       = CCD.CustomerID
+                              INNER JOIN CustomerDemographics CD  ON CCD.CustomerTypeID = CD.CustomerTypeID
+                              WHERE C.CustomerID = @id";
+
+                var customer = (await connection.QueryAsync<Customer, CustomerDemographic, Customer>(
+                    query,
+                    (c, cd) =>
+                    {
+                        c.Demographics.Add(cd);
+                        return c;
+                    },
+                    new { id },
+                    splitOn: "-")).FirstOrDefault();
 
                 return customer;
             }
         }
 
-        public async Task<List<Customer>> GetCustomersAsync(bool includeRelations = false)
+        /// <summary>
+        /// Gets All Customer Records
+        /// </summary>
+        /// <returns>A List of All Customers</returns>
+        public async Task<List<Customer>> GetCustomersAsync()
         {
             using (var connection = _ConnectionManager.GetConnection())
             {
                 await connection.OpenAsync();
 
-                var customers = await connection.QueryAsync<Customer>("SELECT * FROM Customers");
+                var query = @"SELECT C.*, '-', CD.* FROM Customers C
+                              INNER JOIN CustomerCustomerDemo CCD ON C.CustomerID       = CCD.CustomerID
+                              INNER JOIN CustomerDemographics CD  ON CCD.CustomerTypeID = CD.CustomerTypeID";
+
+                // Get Customers and Demographics and Map them to the Data Objects
+                var lookup    = new Dictionary<string, Customer>();
+                var customers = await connection.QueryAsync<Customer, CustomerDemographic, Customer>(
+                    query,
+                    (c, cd) =>
+                    {
+                        Customer customer;
+                        if (!lookup.TryGetValue(c.CustomerID, out customer))
+                        {
+                            lookup.Add(c.CustomerID, c);
+                            customer = c;
+                        }
+
+                        customer.Demographics.Add(cd);
+                        return customer;
+                    }, 
+                    splitOn: "-");
 
                 return customers.ToList();
             }
         }
 
+        /// <summary>
+        /// Adds a New Customer to the Database
+        /// </summary>
+        /// <param name="customer">Customer Record Data to Add</param>
+        /// <returns>True if Successful, False if record with same ID already exists or Add fails</returns>
         public async Task<bool> AddCustomerAsync(Customer customer)
         {
+            // Return False if Customer with Given ID already Exists
             if (await GetCustomerAsync(customer.CustomerID) is not null)
                 return false;
 
+            // Insert the new Customer Record into the Database
             using (var connection = _ConnectionManager.GetConnection())
             {
                 await connection.OpenAsync();
@@ -173,8 +243,18 @@ namespace NorthwindData
             }
         }
 
+        /// <summary>
+        /// Updates an Existing Customer Record with New Data
+        /// </summary>
+        /// <param name="customer">Updated Customer Data</param>
+        /// <returns>True if the Customer was Successfully Updated, False if not exists or Update Fails</returns>
         public async Task<bool> UpdateCustomerAsync(Customer customer)
         {
+            // Return False if Customer Doesn't Exist
+            if (await GetCustomerAsync(customer.CustomerID) is null)
+                return false;
+
+            // Update the Customer
             using (var connection = _ConnectionManager.GetConnection())
             {
                 await connection.OpenAsync();
@@ -198,81 +278,128 @@ namespace NorthwindData
             }
         }
 
+        /// <summary>
+        /// Deletes a Customer with the Given ID from the Database
+        /// </summary>
+        /// <param name="id">ID of the Customer to Delete</param>
+        /// <returns>True if the Delete is Successful or record doesn't exist, False Otherwise</returns>
         public async Task<bool> DeleteCustomerAsync(string id)
         {
+            // Return true if record doesn't exist
+            if (await GetCustomerAsync(id) is null)
+                return true;
+
+            // Delete the Record
             using (var connection = _ConnectionManager.GetConnection())
             {
                 await connection.OpenAsync();
-                var rowsAffected = await connection.ExecuteAsync("DELETE FROM Customers WHERE CustomerID = @id", new { id });
+                var parameters  = new { id };
+                var sql         = @"DELETE FROM Customers WHERE CustomerID = @id";
 
-                return rowsAffected == 1;
+                return await connection.ExecuteAsync(sql, parameters) == 1;
             }
         }
 
-        public async Task<bool> AddCustomerDemographicForCustomerAsync(string customerID, string customerTypeID)
+        /// <summary>
+        /// Associates a CustomerDemographic Record with the Customer with the Given ID
+        /// </summary>
+        /// <param name="customerID">ID of the Customer to Add the Demographic to</param>
+        /// <param name="customerTypeID">ID of the Demographic to Add the Customer to</param>
+        /// <returns>True if Add Succeeds or Demographic is Already Added, False if Demographic Doesn't Exist or Add Fails</returns>
+        public async Task<bool> AddDemographicToCustomer(string customerID, string customerTypeID)
         {
-            var demographicExists = (await GetCustomerDemographicsForCustomerAsync(customerID))
-                .Where(demo => demo.CustomerTypeID == customerTypeID)
-                .FirstOrDefault() is not null;
+            // Return True if Customer Already has Demographic Added
+            if (await CustomerHasDemographic(customerID, customerTypeID))
+                return true;
 
-
-            if (demographicExists)
+            // Return False if Demographic Doesn't Exist
+            if (!await DemographicExistsAsync(customerTypeID))
                 return false;
 
+
+            // Add Demographic to Customer
             using (var connection = _ConnectionManager.GetConnection())
             {
                 await connection.OpenAsync();
-                var sql = @"INSERT INTO CustomerCustomerDemo(CustomerID, CustomerTypeID)
-                            VALUES(@customerID, @customerTypeID)";
+                var parameters  = new { customerID, customerTypeID };
+                var sql         = @"INSERT INTO CustomerCustomerDemo(CustomerID, CustomerTypeID)
+                                    VALUES (@customerID, @customerTypeID)";
 
-                return await connection.ExecuteAsync(sql, new { customerID, customerTypeID }) == 1;
+                return await connection.ExecuteAsync(sql, parameters) == 1;
             }
         }
 
-        public async Task<bool> AddCustomerDemographicForCustomerAsync(string customerID, CustomerDemographics demographics)
+        /// <summary>
+        /// Removes Associated Demographic from Customer
+        /// </summary>
+        /// <param name="customerID">ID of the Customer to Remove Demographic From</param>
+        /// <param name="customerTypeID">ID of the Demographic to Remove</param>
+        /// <returns>True if Delete Succeeds or Customer Doesn't Have Demographic, False if Delete Fails</returns>
+        public async Task<bool> DeleteDemographicFromCustomer(string customerID, string customerTypeID)
         {
-            // Try Adding Non-Existent CustomerDemographics Record
-            if (await GetCustomerDemographicsAsync(demographics.CustomerTypeID) is null)
-                if (!await AddCustomerDemographicsAsync(demographics))
-                    return false;
-            
-            
-            return await AddCustomerDemographicForCustomerAsync(customerID, demographics.CustomerTypeID);
+            // Return True if Customer Doesn't Have Demographic
+            if (!await CustomerHasDemographic(customerID, customerTypeID))
+                return true;
+
+            using (var connection = _ConnectionManager.GetConnection())
+            {
+                await connection.OpenAsync();
+
+                var parameters  = new { customerID, customerTypeID };
+                var sql         = @"DELETE FROM CustomerCustomerDemo WHERE CustomerID = @customerID AND CustomerTypeID = @customerTypeID";
+
+                return await connection.ExecuteAsync(sql, parameters) == 1;
+            }
         }
+
         #endregion
 
         #region Customer Demographics
-        public async Task<CustomerDemographics?> GetCustomerDemographicsAsync(string id)
+        private async Task<bool> DemographicExistsAsync(string customerTypeID)
         {
             using (var connection = _ConnectionManager.GetConnection())
             {
                 await connection.OpenAsync();
-                return await connection.QuerySingleOrDefaultAsync<CustomerDemographics>("SELECT * FROM CustomerDemographics WHERE CustomerTypeID = @id", new { id }); ;
+
+                var parameters  = new { customerTypeID };
+                var sql         = @"SELECT CustomerTypeID FROM CustomerDemographics WHERE CustomerTypeID = @customerTypeID";
+                var result      = await connection.QuerySingleOrDefaultAsync<string>(sql, parameters);
+
+                return !string.IsNullOrWhiteSpace(result);
             }
         }
 
-        public async Task<List<CustomerDemographics>> GetCustomerDemographicsAsync()
+        public async Task<CustomerDemographic?> GetCustomerDemographicsAsync(string id)
         {
             using (var connection = _ConnectionManager.GetConnection())
             {
                 await connection.OpenAsync();
-                return (await connection.QueryAsync<CustomerDemographics>("SELECT * FROM CustomerDemographics")).ToList();
+                return await connection.QuerySingleOrDefaultAsync<CustomerDemographic>("SELECT * FROM CustomerDemographics WHERE CustomerTypeID = @id", new { id }); ;
             }
         }
 
-        public async Task<List<CustomerDemographics>> GetCustomerDemographicsForCustomerAsync(string customerID)
+        public async Task<List<CustomerDemographic>> GetCustomerDemographicsAsync()
+        {
+            using (var connection = _ConnectionManager.GetConnection())
+            {
+                await connection.OpenAsync();
+                return (await connection.QueryAsync<CustomerDemographic>("SELECT * FROM CustomerDemographics")).ToList();
+            }
+        }
+
+        public async Task<List<CustomerDemographic>> GetCustomerDemographicsForCustomerAsync(string customerID)
         {
             using (var connection = _ConnectionManager.GetConnection())
             {
                 await connection.OpenAsync();
                 var demographicIDs = await connection.QueryAsync<string>(               "SELECT CustomerTypeID FROM CustomeCustomerDemo WHERE CustomerID = @customerID" , new { customerID      });
-                var demographics   = await connection.QueryAsync<CustomerDemographics>( "SELECT * FROM CustomerDemographics WHERE CustomerTypeID IN @demographicIDs"    , new { demographicIDs  });
+                var demographics   = await connection.QueryAsync<CustomerDemographic>( "SELECT * FROM CustomerDemographics WHERE CustomerTypeID IN @demographicIDs"    , new { demographicIDs  });
 
                 return demographics.ToList();
             }
         }
 
-        public async Task<bool> AddCustomerDemographicsAsync(CustomerDemographics demographics)
+        public async Task<bool> AddCustomerDemographicsAsync(CustomerDemographic demographics)
         {
             if (await GetCustomerDemographicsAsync(demographics.CustomerTypeID) is not null)
                 return false;
@@ -287,7 +414,7 @@ namespace NorthwindData
             }
         }
 
-        public async Task<bool> UpdateCustomerDemographicsAsync(CustomerDemographics demographics)
+        public async Task<bool> UpdateCustomerDemographicsAsync(CustomerDemographic demographics)
         {
             using (var connection = _ConnectionManager.GetConnection())
             {
